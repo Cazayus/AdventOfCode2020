46961b09286f9dc5a9c90c7baad884e249f7d9d9
DCAZAYUS <daniel.cazayus@cea.fr>
2020-12-06 19:23:46 +0100

    Brushing up Day6

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -6,20 +6,17 @@ fun main() {
 private fun partOne() {
     val input = Utils.getInputAsText("Day6")
     val listAnswersByGroup = input.split("${System.lineSeparator()}${System.lineSeparator()}")
-    val count = listAnswersByGroup.map {
-        it.fold("") { accumulator, char ->
-            if (char.isLetter() && !accumulator.contains(char)) accumulator + char else accumulator
-        }
-    }.sumBy { it.count() }
+    val count = listAnswersByGroup.sumBy {
+        it.lines().joinToString("").toSet().size
+    }
     println(count)
 }
 
 private fun partTwo() {
     val input = Utils.getInputAsText("Day6")
     val listAnswersByGroup = input.split("${System.lineSeparator()}${System.lineSeparator()}")
-    val count = listAnswersByGroup.map {
-        val listAnswerByPerson = it.split(System.lineSeparator())
-        listAnswerByPerson.reduce { value1, value2 -> value1.filter { char -> value2.contains(char) } }.count()
-    }.sum()
+    val count = listAnswersByGroup.sumBy {
+        it.lines().reduce { value1, value2 -> value1.filter { char -> value2.contains(char) } }.count()
+    }
     println(count)
 }
