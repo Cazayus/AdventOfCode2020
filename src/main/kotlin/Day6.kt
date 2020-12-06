b27906e6b5dcff648b4afedaa0e2ec558c333c1f
DCAZAYUS <daniel.cazayus@cea.fr>
2020-12-06 19:35:16 +0100

    Brushing up Day6

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,22 +1,10 @@
 fun main() {
-    partOne()
-    partTwo()
-}
-
-private fun partOne() {
     val input = Utils.getInputAsText("Day6")
     val listAnswersByGroup = input.split("${System.lineSeparator()}${System.lineSeparator()}")
-    val count = listAnswersByGroup.sumBy {
+    println(listAnswersByGroup.sumBy {
         it.lines().joinToString("").toSet().size
-    }
-    println(count)
-}
-
-private fun partTwo() {
-    val input = Utils.getInputAsText("Day6")
-    val listAnswersByGroup = input.split("${System.lineSeparator()}${System.lineSeparator()}")
-    val count = listAnswersByGroup.sumBy {
+    })
+    println(listAnswersByGroup.sumBy {
         it.lines().reduce { value1, value2 -> value1.filter { char -> value2.contains(char) } }.count()
-    }
-    println(count)
+    })
 }
