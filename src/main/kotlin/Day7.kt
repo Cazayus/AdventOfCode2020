17c1734b41b96560ab8d591e4472d16c47484184
DCAZAYUS <daniel.cazayus@cea.fr>
2020-12-08 08:50:41 +0100

    Brushing up Day7

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,8 +1,8 @@
 fun main() {
     val input = Utils.getInputAsList("Day7")
-    val regex = Regex(" bags?")
+    val regex = Regex(" bag[s\\.]*")
     val parsedInput: Map<String, Map<String, Int>> =
-        input.map { it.dropLast(1) }.map { it.replace(regex, "") }.map { it.split(" contain ") }.associateBy({ it[0] }) {
+        input.map { it.replace(regex, "") }.map { it.split(" contain ") }.associateBy({ it[0] }) {
             when {
                 it[1].contains("no other") -> emptyMap()
                 it[1].contains(", ") -> it[1].split(", ").associate { split -> split.split(" ", limit = 2)[1] to split.split(" ", limit = 2)[0].toInt() }
@@ -13,15 +13,14 @@ fun main() {
     println("Puzzle 2 : " + countBagContent("shiny gold", parsedInput))
 }
 
-//val regex = Regex("([a-z]+ [a-z]+) bags contain (([0-9]+) ([a-z]+ [a-z]+) bag[s,.\\s]*)+")
 fun findContainingBag(bagToFind: String, parsedInput: Map<String, Map<String, Int>>): Set<String> {
-    val directlyContainsBagToFindSet = parsedInput.mapNotNull { (key, value) -> if (value.containsKey(bagToFind)) key else null }.toSet()
-    return directlyContainsBagToFindSet + (directlyContainsBagToFindSet.fold(mutableSetOf<String>()) { acc, directlyContainsBagToFind ->
+    val setDirectlyContainingBagToFind = parsedInput.mapNotNull { (key, value) -> if (value.containsKey(bagToFind)) key else null }.toSet()
+    return setDirectlyContainingBagToFind + (setDirectlyContainingBagToFind.fold(mutableSetOf()) { acc, directlyContainsBagToFind ->
         acc.addAll(findContainingBag(directlyContainsBagToFind, parsedInput))
         acc
-    }.ifEmpty { emptySet() })
+    })
 }
 
 fun countBagContent(bagToFind: String, parsedInput: Map<String, Map<String, Int>>): Int {
-    return parsedInput[bagToFind]?.map { (key, value) -> value + value * countBagContent(key, parsedInput) }?.sum() ?: 1
+    return parsedInput[bagToFind]?.map { (key, value) -> value + value * countBagContent(key, parsedInput) }?.sum() ?: throw UnsupportedOperationException()
 }
