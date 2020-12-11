f15dccd22460d9b45ee9aaab08607c825ecbb87b
DCAZAYUS <daniel.cazayus@cea.fr>
2020-12-11 18:40:57 +0100

    Brushing up Day 10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,15 +1,15 @@
 import kotlin.math.pow
 
 fun main() {
-    val input = Utils.getInputAsList("Day10").map { it.toLong() }.sorted().toMutableList()
-    input.add(0, 0L)
+    val input = Utils.getInputAsList("Day10").map { it.toInt() }.sorted().toMutableList()
+    input.add(0, 0)
     input.add((input.maxOrNull() ?: return) + 3)
     var oneDifference = 0L
     var threeDifference = 0L
     input.zipWithNext().forEach {
         when (it.second - it.first) {
-            1L -> oneDifference++
-            3L -> threeDifference++
+            1 -> oneDifference++
+            3 -> threeDifference++
             else -> throw UnsupportedOperationException()
         }
     }
@@ -23,26 +23,26 @@ fun main() {
     // Suite de 5 qui se suivent -> 7 arrangement valide
     // Suite de 6 qui se suivent -> 13 arrangement valide
     // Hypothèse : suite de x qui se suivent  -> ???? choix possibles
-    val map = mutableMapOf<Long, Long>() // in this map we store the number of sublists containing successive values with their size as key
-    var currentSubListSize = 1L
+    val map = mutableMapOf<Int, Int>() // in this map we store the number of sublists containing successive values with their size as key
+    var currentSubListSize = 1
     input.zipWithNext().forEach {
         when (it.second - it.first) {
-            1L -> currentSubListSize++
-            3L -> {
+            1 -> currentSubListSize++
+            3 -> {
                 map[currentSubListSize] = map.getOrDefault(currentSubListSize, 0) + 1
-                currentSubListSize = 1L
+                currentSubListSize = 1
             }
             else -> throw UnsupportedOperationException()
         }
     }
     println("Puzzle 2 : " + map.map { (key, value) ->
         when (key) {
-            1L -> 1.0.pow(value.toInt())
-            2L -> 1.0.pow(value.toInt())
-            3L -> 2.0.pow(value.toInt())
-            4L -> 4.0.pow(value.toInt())
-            5L -> 7.0.pow(value.toInt())
-            6L -> 13.0.pow(value.toInt())
+            1 -> 1.0.pow(value)
+            2 -> 1.0.pow(value)
+            3 -> 2.0.pow(value)
+            4 -> 4.0.pow(value)
+            5 -> 7.0.pow(value)
+            6 -> 13.0.pow(value)
             else -> throw UnsupportedOperationException()
         }
     }.reduce { a, b -> a * b }.toLong())
